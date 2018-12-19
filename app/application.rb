943baa7047ca3f60c144bf1c1c20b2fe62faaf5f
Heidi Hlavinka <hchlav@gmail.com>
2018-12-19 00:30:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,10 +4,16 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
+    if req.path == "/testing"
+      resp.write "Testing"
+    else 
+      resp.write "Route not found"
+      resp.status = 404
+    end 
+    resp.finish
     # if req.path.match (/testing/)
     #   @@items.each do |item|
     #     resp.write
     #
   end
-
 end
