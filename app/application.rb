b6798592a20ffb8e1c13a684b5384908dd89fa21
Heidi Hlavinka <hchlav@gmail.com>
2018-12-19 01:18:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    req.path.match(/items/)
       binding.pry
 
       if @@items.each do |item|
