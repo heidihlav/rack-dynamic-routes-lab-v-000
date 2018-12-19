c30fa405d77870c1fb64d1dfbc0e2a8bc755463a
Heidi Hlavinka <hchlav@gmail.com>
2018-12-19 00:53:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,21 +4,22 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == "/testing"
-      resp.write "Testing"
-    else
-      resp.write "Route not found"
-      resp.status = 404
-    end
 
     if req.path.match(/items/)
       @@items.each do |item|
-        resp.write "#{item}\n"
+        resp.write "#{item.price}\n"
     end
     else
       resp.write "Item not found"
       resp.status = 400
     end
+
+        if req.path == "/testing"
+          resp.write "Testing"
+        else
+          resp.write "Route not found"
+          resp.status = 404
+        end
 
     resp.finish
   end
