1923880c49c11ed6a963ce3046db7658d4e336e8
Heidi Hlavinka <hchlav@gmail.com>
2018-12-19 01:57:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
 
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
-      if name = @@items.find{|i| i.name == item_name}
+      if item = @@items.find{|i| i.name == item_name}
         resp.write item.price
         # binding.pry
       end
